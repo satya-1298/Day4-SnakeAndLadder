72acc554f92438d8648d440e44147dc9b491878f
unknown <satya-1298>
2023-06-19 11:27:21 +0530

    [Satya] Added : The position of players

---
+++ SnakeAndLadder/DiceRolling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class DiceRolling
+    {
+        public void Game()
+        {
+            int position = 0;
+            Console.WriteLine("Position of the player = " + position);
+        }
+    }
+}

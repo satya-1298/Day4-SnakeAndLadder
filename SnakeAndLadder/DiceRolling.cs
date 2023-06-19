a4a5f43591d4eb9649cf1fb406c15f9a1b3dd598
unknown <satya-1298>
2023-06-19 12:31:34 +0530

    [Satya]Added-Player reaching winning position

--- SnakeAndLadder/DiceRolling.cs
+++ SnakeAndLadder/DiceRolling.cs
@@ -12,31 +12,28 @@ namespace SnakeAndLadder
         {
             int position = 0;
             int Count = 0;
-            Random random = new Random();
-            int dice=random.Next(1,7);
-            Console.WriteLine("The dice rolled to "+dice);
-            position += dice;
-            Console.WriteLine("Position of the player = " + position);
-            Random ran = new Random();
-            int c = ran.Next(3);
-           
-            switch (c)
+            int Winning_Position = 100;
+
+            while (position < Winning_Position) 
             {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("The dice rolled to " + dice);
+                position += dice;
+                Console.WriteLine("Position of the player = " + position);
+                if(position == Winning_Position)
+                {
+                    Console.WriteLine("The player poisition is " + (position));
 
-                case 1:
-                    Console.WriteLine("It's ladder");
-                    position += dice;
-                    break;
-                case 2:
-                    Console.WriteLine("It's Sanke");
-                    position -= dice;
-                    break;
-                default:
-                    Console.WriteLine("No Play the player will be in same place");
-                    break;
-
+                    Console.WriteLine("GameOver");
+                }
+                else if(position> Winning_Position) 
+                {
+                    Console.WriteLine("The player not reached 100th position");
+                    Console.WriteLine("The player Remains in poisition  "+(position-dice));
+                }
+               // Console.WriteLine("The player position moved to : " + position);
             }
-            Console.WriteLine("The player position moved to : "+position);
         }
     }
 }
